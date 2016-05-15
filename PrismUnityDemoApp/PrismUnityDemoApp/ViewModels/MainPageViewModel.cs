1dd35057c96062145d2bbf42629d5718d2dbab6b
zhongzf <zhongzf@126.com>
2016-05-15 22:27:33 +0800

    add test cases

--- PrismUnityDemoApp/PrismUnityDemoApp/ViewModels/MainPageViewModel.cs
+++ PrismUnityDemoApp/PrismUnityDemoApp/ViewModels/MainPageViewModel.cs
@@ -16,9 +16,33 @@ namespace PrismUnityDemoApp.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        StudentBody studentBody;
+        public StudentBody StudentBody
+        {
+            protected set { SetProperty(ref studentBody, value); }
+            get { return studentBody; }
+        }
+
         public MainPageViewModel()
         {
+            Init(string.Empty);
+        }
 
+        public void Init(string fileName)
+        {
+            StudentBody = new StudentBody();
+            StudentBody.Students.Add(new Student
+            {
+                FullName = "Adam Harmetz",
+                FirstName = "Adam",
+                PhotoFilename = "http://xamarin.github.io/xamarin-forms-book-samples/SchoolOfFineArt/AdamHarmetz.png"
+            });
+            StudentBody.Students.Add(new Student
+            {
+                FullName = "Alan Brewer",
+                FirstName = "Alan",
+                PhotoFilename = "http://xamarin.github.io/xamarin-forms-book-samples/SchoolOfFineArt/AlanBrewer.png"
+            });
         }
 
         public void OnNavigatedFrom(NavigationParameters parameters)
